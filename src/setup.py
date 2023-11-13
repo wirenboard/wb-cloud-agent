eb7a758e64d91cb9473758e434c996fa71be5177
Alexey Chudin <kazqvaizer@gmail.com>
2023-11-13 20:26:08 +1000

    Better executable name

--- src/setup.py
+++ src/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='wb_cloud_agent',
-    version='0.0.2',
+    version='0.1.0',
     packages=['wb_cloud_agent'],
     entry_points={
-        'console_scripts': ['wb_cloud_agent = wb_cloud_agent.wb_cloud_agent:main']
+        'console_scripts': ['wb-cloud-agent = wb_cloud_agent.wb_cloud_agent:main']
     },
     python_requires='>=3.9.0',
     install_requires=['requests'],
