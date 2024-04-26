88b75dd6b97cdf66baff3afaf26da0aa70e27720
vdromanov <vdromanov@yahoo.com>
2024-04-26 12:12:19 +0300

    move filling config with atecc bus to execstartpre

--- check-certs.sh
+++ check-certs.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+AGENT_CONFIG="/mnt/data/etc/wb-cloud-agent.conf"
+
 print_bundle_part() {
     awk -v "req_part=$1" 'BEGIN { c = 0; } /BEGIN CERT/{c++} c == req_part { print }'
 }
@@ -31,4 +33,14 @@ if [ ! -f "$TARGET_CERT" ] || ! cert_is_valid "$TARGET_CERT"; then
         print_bundle_part 2 < "$ORIGINAL_CERT" > "$TARGET_CERT"
         print_bundle_part 1 < "$ORIGINAL_CERT" >> "$TARGET_CERT"
     fi
+fi
+
+# fix agent config (ATECC path according to device version)
+. /usr/lib/wb-utils/wb_env.sh
+wb_source of
+
+if of_machine_match "wirenboard,wirenboard-720"; then
+    sed -i 's/ATECCx08:00:../ATECCx08:00:02/g' "$AGENT_CONFIG"
+elif of_machine_match "contactless,imx6ul-wirenboard60"; then
+    sed -i 's/ATECCx08:00:../ATECCx08:00:04/g' "$AGENT_CONFIG"
 fi
