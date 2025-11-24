4ef33bf5817fc29d53e6977cdb14426bcfab3f15
smintank <assbestmail@gmail.com>
2025-11-24 13:07:30 +0100

    Add ping utility to check host reachability

---
+++ wb/cloud_agent/handlers/ping.py
@@ -0,0 +1,18 @@
+import logging
+import subprocess
+import time
+
+
+def wait_for_ping(host: str, period: int = 5) -> None:
+    while True:
+        result = subprocess.run(
+            ["ping", "-c", "1", "-W", "2", host],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        )
+        if result.returncode == 0:
+            logging.info("Host %s is reachable", host)
+            return
+
+        logging.warning("Host %s is unreachable. Retrying in %s seconds...", host, period)
+        time.sleep(period)
