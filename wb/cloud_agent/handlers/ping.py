612d64ae845e9f1913dc4b9daf8d603306816301
smintank <assbestmail@gmail.com>
2025-12-01 11:14:17 +0100

    Mute logger messages inside while loop

--- wb/cloud_agent/handlers/ping.py
+++ wb/cloud_agent/handlers/ping.py
@@ -5,15 +5,17 @@ import requests
 
 
 def wait_for_cloud_reachable(url: str, period: int = 5) -> None:
+    logging.info("Trying to reach cloud at '%s' every %s seconds...", url, period)
     while True:
         try:
             response = requests.head(url, timeout=15, allow_redirects=True)
             if 200 <= response.status_code < 400:
                 logging.info("Cloud '%s' is reachable (status %s)", url, response.status_code)
                 return
-            logging.error("Cloud '%s' is unreachable (status %s)", url, response.status_code)
-        except requests.RequestException as exc:
-            logging.exception("Cloud '%s' is unreachable due to exception: %s", url, exc)
 
-        logging.info("Retrying in %s seconds...", period)
+            logging.debug("Cloud '%s' is unreachable (status %s)", url, response.status_code)
+        except requests.RequestException as exc:
+            logging.debug("Cloud '%s' is unreachable due to exception: %s", url, exc)
+
+        logging.debug("Retrying in %s seconds...", period)
         time.sleep(period)
