690d2ea0ac95ff800c94e33c197ecdf90ab6c0c5
smintank <assbestmail@gmail.com>
2026-01-19 12:15:19 +0100

    Restart provider instances after upgrade using deb-systemd-invoke in postinst

--- activate-providers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-if [ -d /etc/wb-cloud-agent/providers ]; then
-    for provider in $(ls /etc/wb-cloud-agent/providers/); do
-        deb-systemd-invoke try-restart wb-cloud-agent@$provider
-    done
-fi
